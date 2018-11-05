64071fe430e81d064d7d8ee359d09754d5fd7081
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-05 23:56:58 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,5 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by {|phrase| ESPERANTO_ALPHABET.index(phrase[0])}
+  arr.sort_by {|phrase| ESPERANTO_ALPHABET.chars.map{|x|, ESPERANTO_ALPHABET.index(x)}}
 end
